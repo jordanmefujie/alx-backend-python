3d63bad4a0d02e74c4d7e16e723219f63d8cfe68
jordanmefujie <mjordanlonpo@gmail.com>
2024-03-22 09:26:06 -0700

    1. Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-
-""" Async Comprehensions """
-
-from asyncio import sleep
-from random import uniform
+"""Task 1's module.
+"""
 from typing import List
+from importlib import import_module as using
 
-async_generator = __import__('0-async_generator').async_generator
+
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ Async Comprehensions  """
-    a = [i async for i in async_generator()]
-    return a
+    """Creates a list of 10 numbers from a 10-number generator.
+    """
+    return [num async for num in async_generator()]
